05801665cfd81afbc90a6c4b63229db0cd2c5e5a
ihebmbarki <iheb.mbarki@esprit.tn>
2024-12-22 15:53:42 +0100

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Iheb Mbarki on 22/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
