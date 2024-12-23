5e04134556427b3e64131b385848863316b0bd7d
ihebmbarki <iheb.mbarki@esprit.tn>
2024-12-23 14:18:13 +0100

    Feat: Project Setup; Built Explore View UI; Created Custom Search Bar

---
+++ Airbnb/Core/Components/ListingImageCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingImageCarouselView.swift
+//  Airbnb
+//
+//  Created by Iheb Mbarki on 23/12/2024.
+//
+
+import SwiftUI
+
+struct ListingImageCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingImageCarouselView()
+}
---
+++ Airbnb/Core/Listings/ListingItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingView.swift
+//  Airbnb
+//
+//  Created by Iheb Mbarki on 22/12/2024.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingItemView()
+}
