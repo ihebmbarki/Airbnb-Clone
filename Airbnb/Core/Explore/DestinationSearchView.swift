334e812aebd4632da1de1cae4e1b73ba7193ffe3
ihebmbarki <iheb.mbarki@esprit.tn>
2024-12-25 16:32:04 +0100

    finalize Search destination view

--- Airbnb/Core/Explore/DestinationSearchView.swift
+++ Airbnb/Core/Explore/DestinationSearchView.swift
@@ -7,12 +7,156 @@
 
 import SwiftUI
 
+enum DestinationSearchOptions {
+    case location
+    case dates
+    case guests
+}
+
 struct DestinationSearchView: View {
+    @Binding var show: Bool
+    @State private var destination = ""
+    
+    @State private var selectedOption: DestinationSearchOptions = .location
+    @State private var startDate = Date()
+    @State private var endDate = Date()
+    @State private var numGuests = 0
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button {
+                withAnimation(.snappy) {
+                    show.toggle()
+                }
+            } label: {
+                Image(systemName: "xmark.circle")
+                    .imageScale(.large)
+                    .foregroundStyle(.black)
+            }
+            
+            //Destination view
+            VStack(alignment: .leading) {
+                if selectedOption == .location {
+                    Text("Where to?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .imageScale(.small)
+                        
+                        TextField("Search destinations",text: $destination)
+                            .font(.subheadline)
+                    }
+                    .frame(height: 40)
+                    .padding(.horizontal)
+                    .overlay() {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(lineWidth: 1.0)
+                            .foregroundStyle(Color(.systemGray4))
+                        
+                    }
+                } else {
+                    CollapsedPickerView(title: "Where", description: "Add destination ")
+                }
+                
+            }
+            .padding()
+            .frame(height: selectedOption == .location ? 120 : 64)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+            
+            .onTapGesture {
+                withAnimation(.snappy) { selectedOption = .location }
+            }
+            
+            //Date selection view
+            VStack(alignment: .leading ) {
+                if selectedOption == .dates {
+                    Text("When's your trip?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    VStack {
+                        DatePicker("From", selection: $startDate, displayedComponents: .date)
+                        
+                        Divider()
+                        
+                        DatePicker("To", selection: $endDate, displayedComponents: .date)
+                    }
+                    .foregroundStyle(.gray)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    
+                } else {
+                    CollapsedPickerView(title: "When", description: "Add dates ")
+                }
+            }
+            .padding()
+            .frame(height: selectedOption == .dates ? 180 : 64)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+            .onTapGesture {
+                withAnimation(.snappy) { selectedOption = .dates }
+            }
+            
+            //Num guests
+            VStack(alignment: .leading) {
+                if selectedOption == .guests {
+                    Text("Who's coming?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    Stepper {
+                        Text("\(numGuests) Adults")
+                    } onIncrement: {
+                        numGuests += 1
+                    } onDecrement: {
+                        guard numGuests > 0 else { return }
+                        numGuests -= 1
+                    }
+                     
+                } else {
+                    CollapsedPickerView(title: "Who", description: "Add guests")
+                }
+            }
+            .padding()
+            .frame(height: selectedOption == .guests ? 120 : 64)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+            .onTapGesture {
+                withAnimation(.snappy) { selectedOption = .guests }
+            }
+        }
     }
 }
 
 #Preview {
-    DestinationSearchView()
+    DestinationSearchView(show: .constant(false))
+}
+
+struct CollapsedPickerView: View {
+    let title: String
+    let description: String
+    
+    var body: some View {
+        VStack {
+            HStack{
+                Text(title)
+                    .foregroundStyle(.gray)
+                
+                Spacer()
+                
+                Text(description)
+            }
+            .fontWeight(.semibold)
+            .font(.subheadline)
+        }
+    }
 }
--- Airbnb/Core/Listings/ListingItemView.swift
+++ Airbnb/Core/Listings/ListingItemView.swift
@@ -1,5 +1,5 @@
 //
-//  ListingView.swift
+//  ListingItemView.swift
 //  Airbnb
 //
 //  Created by Iheb Mbarki on 22/12/2024.
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct ListingItemView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing :8) {
+            //images
+            
+            ListingImageCarouselView()
+            .frame(height: 320)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+           
+            
+            //listing details
+            
+            HStack(alignment: .top) {
+                //details
+                VStack(alignment: .leading) {
+                    Text("Miami, Florida")
+                        .fontWeight(.bold)
+                        .foregroundStyle(.black)
+                    
+                    Text("12 mi away")
+                        .foregroundStyle(.gray)
+                    Text("Nov 3 - 10")
+                        .foregroundStyle(.gray)
+                    
+                    HStack(spacing: 4) {
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        Text("night")
+                    }
+                    .foregroundStyle(.black)
+                }
+                
+                Spacer()
+                
+                //rating
+                HStack(spacing: 2) {
+                    Image(systemName: "star.fill")
+                    
+                    Text("4.86")
+                }
+                .foregroundStyle(.black)
+            }
+            .font(.footnote)
+        }
     }
 }
 
